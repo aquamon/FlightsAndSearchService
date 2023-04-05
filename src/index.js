1b7e34dfb60481cc7aa090e1f31cf20dd9168660
Monash Chhetri <monash.chhetri22@gmail.com>
2023-04-05 15:00:47 +0530

    Setup basic dotenv

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const {PORT} = require('./config/serverConfig');
+
+
+const setupAndStartServer = async () => {
+    
+    const app = express();
+
+    app.listen(PORT,()=>{
+        console.log(`Server started on port ${PORT}.`);
+        // console.log(process.env);
+    });
+}
+
+setupAndStartServer();
